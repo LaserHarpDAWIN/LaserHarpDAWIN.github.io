36243372e755fb881087f3886a4bda8800ef79f8
hommes_doutant <thomas.dumonet@gmail.com>
2015-01-28 17:42:04 +0100

    ajout d'un article

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -48,8 +48,9 @@ $(document).ready(function() {
   });
 
   $(document).scroll(function() {
-    console.log($(document).scrollTop());
+    if ($(document).scrollTop()<=400) {
     var scroll = ($(document).scrollTop())/1.61803398875;
     $('.entry-image').css('top', scroll);
+    };
 });
 });
